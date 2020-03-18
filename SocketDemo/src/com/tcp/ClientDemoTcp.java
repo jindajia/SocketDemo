22c821f4368f8c78792e26edfef48300d66381ec
Jiajinda <13766785543@163.com>
2020-03-18 22:19:29 +0800

    添加了tcp连接的实现

---
+++ SocketDemo/src/com/tcp/ClientDemoTcp.java
@@ -0,0 +1,20 @@
+package com.tcp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+//用tcp建立连接发送数据
+public class ClientDemoTcp {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		//1、创建socket对象
+		Socket socket = new Socket("127.0.0.1",10000);
+		//2、获取一个IO流开始写数据
+		OutputStream os = socket.getOutputStream();
+		os.write("hello tcp".getBytes());
+		//3、释放资源
+		os.close();
+		socket.close();
+	}
+}
---
+++ SocketDemo/src/com/tcp/SeverDemoTcp.java
@@ -0,0 +1,29 @@
+package com.tcp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SeverDemoTcp {
+
+	public static void main(String[] args) throws IOException {
+		//1、创建socket对象
+		ServerSocket ss = new ServerSocket(10000);
+		//2、调用accept等待客户端连接
+		System.out.println("服务器等待建立TCP连接");
+		Socket socket = ss.accept();//阻塞在这里
+		System.out.println("服务器已经建立TCP连接");
+		//3、获得输入流对象
+		InputStream is = socket.getInputStream();
+		int b;
+		while((b = is.read())!=-1) {
+			System.out.print((char)b);
+		}
+		//4、释放资源
+		is.close();
+		ss.close();
+		System.out.println("服务器TCP连接已经释放");
+	}
+
+}
